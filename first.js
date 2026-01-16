fd42ba176c4ab01c84d9c9bf6b3320fd0c291b8e
dhaygudeshweta <shwetadhaygude97@gmail.com>
2026-01-16 09:18:13 +0530

    if-else program

--- first.js
+++ first.js
@@ -1,8 +1,20 @@
-let num=5;
-if(num%2===0)
-{
-    console.log( num," is even");
-}
-else{
-    console.log( num, "is odd");
-}
+// let num=5;
+// if(num%2===0)
+// {
+//     console.log( num," is even");
+// }
+// else{
+//     console.log( num, "is odd");
+// }
+ let mode="Silver";
+ let color;
+ if(mode=== "Dark")
+ {
+    color="Black";
+ }else if(mode==="blue")
+ {
+    color="blue";
+ }else{
+    color="light";
+ }
+ console.log(color);
