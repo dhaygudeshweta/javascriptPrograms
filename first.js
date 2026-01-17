23c18d19027f1504f7091975dd1790280d3a979c
dhaygudeshweta <shwetadhaygude97@gmail.com>
2026-01-17 13:34:49 +0530

    prompt_use

--- first.js
+++ first.js
@@ -6,15 +6,26 @@
 // else{
 //     console.log( num, "is odd");
 // }
- let mode="Silver";
- let color;
- if(mode=== "Dark")
- {
-    color="Black";
- }else if(mode==="blue")
- {
-    color="blue";
- }else{
-    color="light";
- }
- console.log(color);
+//  let mode="Silver";
+//  let color;
+//  if(mode=== "Dark")
+//  {
+//     color="Black";
+//  }else if(mode==="blue")
+//  {
+//     color="blue";
+//  }else{
+//     color="light";
+//  }
+//  console.log(color);
+let num=prompt("enter a number");
+
+if (num%5===0)
+{
+   console.log(num,"number is multiply by 5");
+
+}else
+{
+   console.log(num,"number is  NOT multiply by 5 ");
+}
+
