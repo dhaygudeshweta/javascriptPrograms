3a8229a8b8cdd340d30d48808221d927db960859
dhaygudeshweta <shwetadhaygude97@gmail.com>
2026-01-19 07:48:56 +0530

    program for guessing the write number game

--- Loops.js
+++ Loops.js
@@ -1,10 +1,22 @@
+//prorgam for even numbers 
+// for(let i=0 ;i<=100;i++)
+// {
+//     if(i%2===0)
+//     {
+// console.log("i is=",i);
+//     }
+    
+    
+// }
+// 
 
-for(let i=0 ;i<=100;i++)
+
+// program for guessing the write number game
+
+let gameNum=20;
+let userNum=prompt("Guess thenumber");
+while(userNum!=gameNum)
 {
-    if(i%2===0)
-    {
-console.log("i is=",i);
-    }
-    
-    
+    userNum=prompt("you entered wrong number");
 }
+console.log("congratulations you enterd right number");
