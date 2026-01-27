0192a7577069842f73b8836490d874adbfe1cd04
dhaygudeshweta <shwetadhaygude97@gmail.com>
2026-01-27 17:29:17 +0530

    DOM attributes and Element Practice

--- script.js
+++ script.js
@@ -20,11 +20,37 @@
 // console.dir(div);
 
 
-let h2=document.querySelector("h2");
-console.dir(h2.innerText);
-h2.innerText=h2.innerText+ " from shweta kakade";
+// let h2=document.querySelector("h2");
+// console.dir(h2.innerText);
+// h2.innerText=h2.innerText+ " from shweta kakade";
 
 
 
 
+// let para=document.querySelector("p");
+// console.log(para.getAttribute("class"));
 
+// let dev=document.querySelector("div");
+// console.log(dev.getAttribute("class"));
+
+
+// let newbtn=document.createElement("button");
+// newbtn.innerText="click me";
+// console.log(newbtn);
+
+
+// let div=document.querySelector("div");
+// div.prepend(newbtn);
+
+// div.append(newbtn);
+
+
+//create button element give it to text style and add into body
+let btn=document.createElement("button");
+btn.innerText="click me";
+btn.style.backgroundColor="red";
+btn.style.color="white";
+ console.log(btn);
+
+
+document.querySelector("body").prepend(btn);
