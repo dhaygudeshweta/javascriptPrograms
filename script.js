de1b14fdff9b8b8c91b911b4d10dff86c8ad4283
dhaygudeshweta <shwetadhaygude97@gmail.com>
2026-01-27 13:25:23 +0530

    practice Dom manipulation and properties

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-let headear=document.getElementById("heading");
-console.log(headear);
- let headings=document.getElementsByClassName("heading2");
- console.log(headings);
+// let headear=document.getElementById("heading");
+// console.log(headear);
+//  let headings=document.getElementsByClassName("heading2");
+//  console.log(headings);
 
 
- let first =document.querySelector("#heading");
- console.log(first);
+//  let first =document.querySelector("#heading");
+//  console.log(first);
 
- let firstclass=document.querySelectorAll(".heading2");
- console.log(firstclass);
+//  let firstclass=document.querySelectorAll(".heading2");
+//  console.log(firstclass);
+
+// let firs=document.getElementById("list");
+// console.log(firs.firstElementChild.textContent);
+
+
+
+// // 
+// let div=document.querySelector("div");
+// console.dir(div);
+
+
+let h2=document.querySelector("h2");
+console.dir(h2.innerText);
+h2.innerText=h2.innerText+ " from shweta kakade";
+
+
+
+
+
