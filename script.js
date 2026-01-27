64adbe00ad13f2c8f7629bc8e7f28008c48e3082
dhaygudeshweta <shwetadhaygude97@gmail.com>
2026-01-27 12:27:59 +0530

    Dom manipulation practice

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("DOM Practice");
+let headear=document.getElementById("heading");
+console.log(headear);
+ let headings=document.getElementsByClassName("heading2");
+ console.log(headings);
+
+
+ let first =document.querySelector("#heading");
+ console.log(first);
+
+ let firstclass=document.querySelectorAll(".heading2");
+ console.log(firstclass);
